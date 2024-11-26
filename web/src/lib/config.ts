a4979bd95ceb61fee3d9731ae8f29b7c3a696bd3
jacksturt <jack@sturt.io>
2024-11-25 23:44:55 -0700

    ayaya

--- web/src/lib/config.ts
+++ web/src/lib/config.ts
@@ -1,3 +1,3 @@
-const WEBHOOK_SECRET = "f05c0052b60d832e1c5499a82a91b9209000168b70efed9b7b0e362acd183857";
+const WEBHOOK_SECRET = "";
 const RESIZER_URL = "https://banger-resizer-production.up.railway.app";
 export { WEBHOOK_SECRET, RESIZER_URL };
