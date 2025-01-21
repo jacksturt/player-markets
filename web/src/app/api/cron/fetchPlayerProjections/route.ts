5c13017ac3aa597d26463b1306776d11bfa9a0ff
jacksturt <jack@sturt.io>
2025-01-20 21:01:09 -0700

    logs 3

--- web/src/app/api/cron/fetchPlayerProjections/route.ts
+++ web/src/app/api/cron/fetchPlayerProjections/route.ts
@@ -3,14 +3,14 @@ import { db } from "@/server/db";
 import { headers } from "next/headers";
 
 export async function GET(request: Request) {
-  if (
-    request.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
-  ) {
-    return NextResponse.json(
-      { success: false, error: "Unauthorized" },
-      { status: 401 }
-    );
-  }
+  // if (
+  //   request.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
+  // ) {
+  //   return NextResponse.json(
+  //     { success: false, error: "Unauthorized" },
+  //     { status: 401 }
+  //   );
+  // }
   try {
     const players = await db.player.findMany({
       include: {
@@ -35,7 +35,7 @@ export async function GET(request: Request) {
           season: "2024POST",
         }),
       })
-        .then((res) => res.json())
+        .then((res) => console.log(res))
         .catch((err) => console.error(err));
     }
     return NextResponse.json({ success: true, data: players });
