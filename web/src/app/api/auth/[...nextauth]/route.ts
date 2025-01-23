f75980704f8c22de8dc52f0340e165942cea2ba6
jacksturt <jack@sturt.io>
2025-01-23 11:29:26 +0100

    what is going on

--- web/src/app/api/auth/[...nextauth]/route.ts
+++ web/src/app/api/auth/[...nextauth]/route.ts
@@ -4,8 +4,3 @@ import { authConfig } from "@/server/auth/config";
 
 const handler = NextAuth(authConfig);
 export { handler as GET, handler as POST };
-
-export function getServerSideProps(context: any) {
-  console.log("Query params:", context.query);
-  return { props: {} };
-}
