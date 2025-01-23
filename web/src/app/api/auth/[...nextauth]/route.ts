a3cdcf469451eb1f9bcf83f31dc7bfe4ac764709
jacksturt <jack@sturt.io>
2025-01-23 11:24:39 +0100

    gssp

--- web/src/app/api/auth/[...nextauth]/route.ts
+++ web/src/app/api/auth/[...nextauth]/route.ts
@@ -4,3 +4,8 @@ import { authConfig } from "@/server/auth/config";
 
 const handler = NextAuth(authConfig);
 export { handler as GET, handler as POST };
+
+export function getServerSideProps(context: any) {
+  console.log("Query params:", context.query);
+  return { props: {} };
+}
