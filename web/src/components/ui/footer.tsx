f7b719e1d9b94e94de0468aab73c7e1e43ceb3fc
Matt Pfeifer <matthewcpfeifer@gmail.com>
2025-01-23 22:13:53 -0500

    chore: add links to home and wallet on footer

--- web/src/components/ui/footer.tsx
+++ web/src/components/ui/footer.tsx
@@ -1,13 +1,22 @@
+"use client";
+
 import React from "react";
 import HomeIcon from "../icons/home";
 import Image from "next/image";
+import WalletIcon from "../icons/wallet";
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <footer className="z-20 fixed bottom-0 mx-auto left-0 right-0 max-w-[430px] h-16 rounded-t-[32px] bg-black">
       <div className="relative w-full h-full flex items-center justify-evenly">
-        <HomeIcon size={36} />
+        <Link href="/home">
+          <HomeIcon size={36} />
+        </Link>
         <HomeIcon size={36} className="invisible" />
-        <HomeIcon size={36} />
+        <Link href="/home/wallet">
+          <WalletIcon size={36} />
+        </Link>
         <Image
           src="/chat.png"
           alt="Main Chat"
