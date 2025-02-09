dc30e4952f00de7f61e4b937cb54e869ba21933e
jacksturt <jack@sturt.io>
2025-02-08 17:24:40 -0700

    root is annoying

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,15 +1,6 @@
 "use client";
 import DashboardFeature from "@/components/dashboard/dashboard-feature";
-import { useRouter } from "next/router";
-import { useSession } from "next-auth/react";
 
 export default function Page() {
-  const { data: session } = useSession();
-  const router = useRouter();
-
-  if (!session) {
-    router.push(`/auth/signin?callbackUrl=/home`);
-    return null;
-  }
   return <DashboardFeature />;
 }
